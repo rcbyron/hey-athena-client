32b4a9ca6fc9aa96ca793fc99e0caf1654665925
Sahil <sahilshah@flash.net>
2016-01-07 20:40:49 -0600

    Now able to add your own music directory

--- client/modules/active/music.py
+++ client/modules/active/music.py
@@ -3,6 +3,9 @@ Created on Aug 13, 2015
 
 @author: Connor
 '''
+from os import listdir
+from os.path import isfile, join
+import random
 from client.classes.module import Module
 from client.classes.task import ActiveTask
 from client.tts import play_mp3
@@ -20,11 +23,20 @@ class PlaySongTask(ActiveTask):
     
     def action(self, text):
         self.speak("Turning up...")
-        response = input('Do you want to specify a song? Y/N: ')
+        response = input('Do you want to specify a directory? Y/N: ')
         if response == 'Y':
-            path = input('Please specify your music path: ')
-            song = input('What song do you want to be played?: ')
-            play_mp3(song,path)
+            dir = input('What is your music directory: ')
+            shuffle = input('Do you want a specific song? Y/N: ')
+            if shuffle == 'Y':
+                song = input('What song do you want to be played?: ')
+                play_mp3(song, dir) 
+            else:
+                songs = [f for f in listdir(dir) if isfile(join(dir, f))]
+                while 1:
+                    song = random.choice(songs)
+                    while song.endswith(".mp3"):
+                        play_mp3(song,dir)
+                        song = random.choice(songs)
         else:
             play_mp3("limbo.mp3")
         
