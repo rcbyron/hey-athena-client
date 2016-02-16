7da3f6a74f9d8b8d5f45924cacedcd7da1e7fa41
Connor <rcbyron@utexas.edu>
2016-02-15 22:46:31 -0600

    Simple mic_test.py fix

--- athena/tests/mic_test.py
+++ athena/tests/mic_test.py
@@ -3,6 +3,8 @@
 """
 import traceback
 
+from athena import settings
+
 print('---- Running Microphone Test ----')
 
 try:
@@ -16,7 +18,7 @@ try:
         print('\n~ Type \'q\' at any time to quit')
         text = input('~ Test Active or Passive Listening? (A/P): ')
         if 'p' in text.lower():
-            print('\n~ Wake Up Word: \''+stt.WAKE_UP_WORD+'\'')
+            print('\n~ Wake Up Word: \''+settings.WAKE_UP_WORD+'\'')
             stt.listen_keyword()
             print('~ Wake Up Word Detected!')
         elif 'a' in text.lower():
